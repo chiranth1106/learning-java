f2abf29cc003d077149759d982ff4a82cf3000f7
chiranth1106 <102595545+chiranth1106@users.noreply.github.com>
2025-05-06 14:27:22 +0530

    /* */

--- new java/src/armstrong_number/armstrong_number.java
+++ new java/src/armstrong_number/armstrong_number.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class armstrong_number {
 
+    // THE SUM OF NUMBER TO THE POWER OF THE NUMBER OF DIGITS PRESENT IN THE NUMBER 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
