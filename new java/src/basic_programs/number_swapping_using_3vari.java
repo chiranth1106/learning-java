d3c70f4db0e1e2fa6a3f128c4372acfdfe091f66
chiranth1106 <102595545+chiranth1106@users.noreply.github.com>
2025-05-15 13:29:02 +0530

    swapping using 3 variables

---
+++ new java/src/basic_programs/number_swapping_using_3vari.java
@@ -0,0 +1,14 @@
+package basic_programs;
+
+public class number_swapping_using_3vari {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a;
+        a = b;
+        b = c;
+       System.out.println(a+ ", " +b); 
+    }
+    
+}
