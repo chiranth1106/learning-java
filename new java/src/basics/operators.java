8fe11cfc221070e5389ba865ce3a513c48fc0637
chiranth1106 <102595545+chiranth1106@users.noreply.github.com>
2025-05-17 12:38:47 +0530

    operators

---
+++ new java/src/basics/operators.java
@@ -0,0 +1,72 @@
+package basics;
+// import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        // System.out.println(10 == 8 ? "equal":"not equal");
+
+        // int a = 10;
+        // int b = 8;
+        // int c = 15;
+        // if( a > b && a > c){
+        //     System.out.println(a+ ": a is greater");
+        // } else if(b > c){
+        //     System.out.println(b + ": b is greater");
+        // } else {
+        //     System.out.println(c+ ": c is greater");
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a choice : " );
+        // int choice = sc.nextInt();
+        // switch (choice) {
+        //     case 1:
+        //         System.out.println("one");
+        //         break;
+        //     case 2:
+        //         System.out.println("two");
+        //         break;
+
+        //     default:
+        //     System.out.println("enter either 1 or 2");
+        //         break;
+        // }
+
+        // sc.close();
+
+       for(int i = 1; i <= 10; i++){
+        int res = 2 * i;
+        System.out.println("2"+ "*"+ i+ "="+ res);
+       }
+    }
+
+}
+
+
+/* types of operators */
+// -> unary -> increment/decrement -> (++, --)
+// -> binary -> arithmetic/logical/relational -> ((+, -, *, / , %), (&&, ||, |), (>, <, <=, >=, ==, !=))
+// -> tarnary -> conditional => (op1 ? op1 : op2)
+
+
+
+/* conditional statements/control statements */
+// if, if else, else if, switch
+// -> a set of statement is executing if and only if the condition is satisfied
+
+/* if */
+//the set of statement is executing only if condition is satisfied or it will never exicute
+
+/* if else */
+// a set stament is execute if the statement is satisfied otherwise else statement will be executing
+
+/* else if */
+//multiple checking condition
+
+/* switch case */
+
+
+
+/* looping */
+// for, foreach, while, do while
+// a set of statement is executing repeatedly for certain number of times
