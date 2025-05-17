9ad071fc3384acdba056ae9032224f84a8207dcb
chiranth1106 <102595545+chiranth1106@users.noreply.github.com>
2025-05-17 12:38:01 +0530

    identifiers

---
+++ new java/src/basics/identifiers.java
@@ -0,0 +1,13 @@
+package basics;
+public class identifiers {
+    
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        int f_n = 10;
+    }
+}
+
+/* rules for identifiers */
+// should not begin with digits
+// should not have special charater except $ and _
+// should not use keywords 
